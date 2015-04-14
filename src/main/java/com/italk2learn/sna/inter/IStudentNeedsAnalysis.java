6883b6989d55c62fb3dd834e08d2017ef4d1d2c9
Jose Luis Fernandez <jlfg@dcs.bbk.ac.uk>
2015-04-14 13:15:50 +0100

    Second commit

--- src/main/java/com/italk2learn/sna/inter/IStudentNeedsAnalysis.java
+++ src/main/java/com/italk2learn/sna/inter/IStudentNeedsAnalysis.java
@@ -1,12 +1,14 @@
 package com.italk2learn.sna.inter;
 
+import java.sql.Timestamp;
+
 public interface IStudentNeedsAnalysis {
 	
 	public void sendFeedbackTypeToSNA(String feedbackType);
 	
 	public void setAudio(byte[] currentAudioStudent);
 	
-	public void calculateNextTask();
+	public void calculateNextTask(int whizzStudID, String whizzPrevContID, int prevScore, Timestamp timestamp, String WhizzSuggestion, boolean Trial);
 	
 	public String getNextTask();
 	
