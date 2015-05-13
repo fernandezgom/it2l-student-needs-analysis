9aa4ac1ac9f28a6042a8c680ea04ad9a479d851a
Sergio Gutierrez-Santos (Sergut) <sergut@gmail.com>
2015-05-13 19:44:05 +0100

    Added an enumerated type for security.

--- src/main/java/com/italk2learn/sna/VPSequencer.java
+++ src/main/java/com/italk2learn/sna/VPSequencer.java
@@ -7,19 +7,19 @@ import MFSeq.WhizzSequencer;
 
 import com.italk2learn.sna.inter.Sequencer;
 
-
-
 public class VPSequencer implements Sequencer {
 
 	public VPSequencer() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	
-	public String next (int whizzStudID, String whizzPrevContID, int prevScore, Timestamp timestamp, String WhizzSuggestion, int Trial, boolean type) {
-			if (type == false)
-				return FTSequencer.next(whizzStudID, whizzPrevContID, prevScore, timestamp, WhizzSuggestion, Trial);
-			else 
-				return WhizzSequencer.next(whizzStudID, whizzPrevContID, prevScore, timestamp, WhizzSuggestion, Trial);
+	public String next (int whizzStudID, String whizzPrevContID, int prevScore, Timestamp timestamp, String WhizzSuggestion, int Trial, StructuredActivityType type) {
+	    if (type == StructuredActivityType.FRACTIONS_TUTOR) {
+		  return FTSequencer.next(whizzStudID, whizzPrevContID, prevScore, timestamp, WhizzSuggestion, Trial);
+	    } else if (type == StructuredActivityType.WHIZZ) {
+		  return WhizzSequencer.next(whizzStudID, whizzPrevContID, prevScore, timestamp, WhizzSuggestion, Trial);
+	    } else {
+		  throw new IllegalStateException("Invalid structured activity type: " + type);
+	    }
 	}
 }
